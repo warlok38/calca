260b6a8ce43d5bcaf28fd5682c1ec1630c3744b6
warlok38 <warlok38@mail.ru>
2021-10-31 23:57:09 +0700

    верстка интерфейса альфа версия

--- src/features/main/index.jsx
+++ src/features/main/index.jsx
@@ -1,29 +1,78 @@
-import React from 'react';
-import { Form, Input, Button } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Form, InputNumber, Button, Radio, Image } from 'antd';
+import shema from '../../pictures/shema.jpg';
 import * as S from './styled';
 
 export const Main = () => {
+    const u12 = 10;
+    const u24 = 24;
+    const rules = [{ required: true }];
+
     const [form] = Form.useForm();
-    const onFinish = ({ r1, r2 }) => {
-        console.log(r1, r2);
+    const [value, setValue] = useState(u12);
+    const [rd, setRd] = useState(0);
+    const [w, setW] = useState(0);
+
+    const onFinish = ({ u, i, r1, r2, rAup }) => {
+        const rd = (u - 1) / i - (r2 + r1 + rAup);
+        const w = Math.pow(27 / (rAup + r1 + r2 + rd), 2) * rd;
+        setRd(rd);
+        setW(w);
+        form.resetFields();
+    };
+    const onChange = (e) => {
+        setValue(e.target.value);
+    };
+    const onReset = () => {
         form.resetFields();
     };
 
+    useEffect(() => {
+        form.resetFields();
+    }, [form]);
+
     return (
         <S.Wrapper>
-            <Form form={form} onFinish={onFinish} layout="vertical">
-                <Form.Item name="r1">
-                    <Input />
-                </Form.Item>
-                <Form.Item name="r2">
-                    <Input />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Рассчитать
-                    </Button>
-                </Form.Item>
-            </Form>
+            <S.FormWrapper>
+                <Form
+                    form={form}
+                    onFinish={onFinish}
+                    layout="vertical"
+                    initialValues={{ u: value }}
+                >
+                    <S.FormItem name="u" label="U Ип min">
+                        <Radio.Group onChange={onChange} value={value}>
+                            <Radio value={u12}>12</Radio>
+                            <Radio value={u24}>24</Radio>
+                        </Radio.Group>
+                    </S.FormItem>
+                    <S.FormItem name="i" label="Iсраб" rules={rules}>
+                        <InputNumber step="0.1" />
+                    </S.FormItem>
+                    <S.FormItem name="r1" label="R1" rules={rules}>
+                        <InputNumber step="0.01" />
+                    </S.FormItem>
+                    <S.FormItem name="r2" label="R2" rules={rules}>
+                        <InputNumber step="0.01" />
+                    </S.FormItem>
+                    <S.FormItem name="rAup" label="Rауп" rules={rules}>
+                        <InputNumber step="0.1" />
+                    </S.FormItem>
+                    <S.FormItem>
+                        <Button type="primary" htmlType="submit">
+                            Рассчитать
+                        </Button>
+                    </S.FormItem>
+                    <S.FormItem>
+                        <Button onClick={onReset}>Очистить</Button>
+                    </S.FormItem>
+                </Form>
+            </S.FormWrapper>
+            <S.Container>
+                <Image src={shema} />
+                <div>Rогр = {rd}</div>
+                <div>Wогр = {w}</div>
+            </S.Container>
         </S.Wrapper>
     );
 };
--- src/features/main/styled.js
+++ src/features/main/styled.js
@@ -1,3 +1,28 @@
 import styled from 'styled-components';
+import { Form as AntdForm } from 'antd';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+    padding: 24px;
+    display: flex;
+`;
+
+export const FormWrapper = styled.div``;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 24px;
+`;
+
+export const FormItem = styled(AntdForm.Item)`
+    &&& {
+        margin-bottom: 16px;
+        .ant-form-item-required::before {
+            display: none;
+        }
+        .ant-form-item-explain {
+            display: none;
+        }
+    }
+`;
